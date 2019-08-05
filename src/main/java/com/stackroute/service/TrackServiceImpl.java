18932c17fcacd85917cfdbaa1e074e89a1790c73
Clinton Barlow <51949994+Clinton112@users.noreply.github.com>
2019-08-05 23:07:04 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -12,8 +12,8 @@ import java.util.Optional;
 @Service
 public class TrackServiceImpl implements TrackService {
 
-    TrackRepository trackRepository;
-    Track track;
+    private TrackRepository trackRepository;
+    private Track track;
 
     @Autowired
     public TrackServiceImpl(TrackRepository trackRepository) {
@@ -23,11 +23,9 @@ public class TrackServiceImpl implements TrackService {
     @Override
     public Track saveTrack(Track track) throws TrackAlreadyExistsException {
         if (trackRepository.existsById(track.getId())) {
-
             throw new TrackAlreadyExistsException("Track already exists with id" );
         }
-        Track savedTrack = trackRepository.save(track);
-
+       Track savedTrack = trackRepository.save(track);
         return savedTrack;
     }
 
@@ -38,11 +36,8 @@ public class TrackServiceImpl implements TrackService {
 
     @Override
     public Track getTrackById(int id) {
-
-
         Track track = trackRepository.findById(id).get();
         return track;
-
     }
 
     @Override
@@ -51,20 +46,16 @@ public class TrackServiceImpl implements TrackService {
         {
             throw new TrackNotFoundException("Track not found");
         }
-
         return trackRepository.delete(getTrackById(id));
-
     }
 
     @Override
     public Track updateTrack(int id,String comment) {
-
         Optional<Track> track = trackRepository.findById(id);
         Track track1 = track.get();
         track1.setComment(comment);
         Track savedTrack = trackRepository.save(track1);
         return savedTrack;
-
     }
 
     @Override
@@ -76,8 +67,4 @@ public class TrackServiceImpl implements TrackService {
     public List<Track> getTrackByNameSortByName(String name) {
         return trackRepository.findByNameSortedById(name);
     }
-
-
-
-
 }
