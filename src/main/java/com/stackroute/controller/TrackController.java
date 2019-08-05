cb5303dfa755b3f62b3d9d7d6a8b509a24cdb758
Clinton Barlow <51949994+Clinton112@users.noreply.github.com>
2019-08-05 22:45:30 +0530

    Update TrackController.java

--- src/main/java/com/stackroute/controller/TrackController.java
+++ src/main/java/com/stackroute/controller/TrackController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value="api/v1")
 public class TrackController {
 
-    TrackService trackService;
+    private TrackService trackService;
     
     @Autowired
     public TrackController(TrackService trackService) {
@@ -21,44 +21,32 @@ public class TrackController {
 
     @PostMapping("track")
     public ResponseEntity<?> saveTrack(@RequestBody Track track) {
-
         ResponseEntity responseEntity;
         try {
             trackService.saveTrack(track);
             responseEntity = new ResponseEntity("Successfully created", HttpStatus.CREATED);
         }
-
         catch(TrackAlreadyExistsException ex) {
             responseEntity = new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
         }
-
         return responseEntity;
-
     }
 
     @DeleteMapping(value = "/delete/{id}")
     public ResponseEntity<?> deleteTrack(@PathVariable Integer id) {
-
         ResponseEntity responseEntity;
         try{
-
             trackService.deleteTrack(id);
             responseEntity = new ResponseEntity("Delete Successfull", HttpStatus.NO_CONTENT);
-
         }
-
         catch (TrackNotFoundException ex) {
-
             responseEntity = new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
         }
-
         return responseEntity;
-
     }
 
     @PutMapping(value = "/update/{id}/{comment}")
     public ResponseEntity<?> updateTrack(@PathVariable int id, @PathVariable String comment) {
-
         ResponseEntity responseEntity;
         try {
             trackService.updateTrack(id,comment);
@@ -67,17 +55,12 @@ public class TrackController {
             responseEntity = new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
         }
         return responseEntity;
-
     }
-
-
 
     @GetMapping("track")
     public ResponseEntity<?> getAllTracks() {
         System.out.println(trackService.getByTrackName("good").toString());
         System.out.println(trackService.getTrackByNameSortByName("good").toString());
         return new ResponseEntity<>(trackService.getAllTracks(), HttpStatus.OK);
-
     }
-
 }
