4ed6b3cb3846a0e143bdafa59abed71100802008
Clinton Barlow <51949994+Clinton112@users.noreply.github.com>
2019-08-07 12:01:32 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -1,10 +1,9 @@
 package com.stackroute.service;
 
 import com.stackroute.domain.Track;
-import com.stackroute.exceptions.TrackAlreadyExistsException;
-import com.stackroute.exceptions.TrackNotFoundException;
 import com.stackroute.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -12,8 +11,7 @@ import java.util.Optional;
 @Service
 public class TrackServiceImpl implements TrackService {
 
-    private TrackRepository trackRepository;
-    private Track track;
+   private TrackRepository trackRepository;
 
     @Autowired
     public TrackServiceImpl(TrackRepository trackRepository) {
@@ -21,11 +19,7 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public Track saveTrack(Track track)  {
-        if (trackRepository.existsById(track.getId())) {
-
-            throw new TrackAlreadyExistsException("Track already exists with id" );
-        }
+    public Track saveTrack(Track track) {
         Track savedTrack = trackRepository.save(track);
         return savedTrack;
     }
@@ -36,36 +30,30 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public Track getTrackById(int id){
+    public Track getTrackById(int id) {
+
         Track track = trackRepository.findById(id).get();
         return track;
+
     }
 
     @Override
-    public Track deleteTrack(int id) {
-        if(!trackRepository.existsById(id))
-        {
-            throw new TrackNotFoundException("Track not found");
-        }
-       return trackRepository.delete(getTrackById(id));
+    public void deleteTrack(int id) {
+
+        trackRepository.delete(getTrackById(id));
+
     }
 
     @Override
     public Track updateTrack(int id,String comment) {
+
         Optional<Track> track = trackRepository.findById(id);
         Track track1 = track.get();
         track1.setComment(comment);
         Track savedTrack = trackRepository.save(track1);
         return savedTrack;
+
     }
 
-    @Override
-    public List<Track> getByTrackName(String name) {
-        return trackRepository.findByName(name);
-    }
 
-    @Override
-    public List<Track> getTrackByNameSortByName(String name){
-        return trackRepository.findByNameSortedById(name);
-    }
 }
