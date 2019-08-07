2471d7ed3d14fa61f989062a5af4c39f68550423
Clinton Barlow <51949994+Clinton112@users.noreply.github.com>
2019-08-07 11:49:30 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -16,7 +16,7 @@ public class TrackServiceImpl implements TrackService {
     private Track track;
 
     @Autowired
-    public TrackServiceImpl(TrackRepository trackRepository) {
+    public TrackServiceImpl(TrackRepository trackRepository){
         this.trackRepository = trackRepository;
     }
 
@@ -30,12 +30,12 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public List<Track> getAllTracks() {
+    public List<Track> getAllTracks()throws TrackNotFoundException {
         return trackRepository.findAll();
     }
 
     @Override
-    public Track getTrackById(int id) {
+    public Track getTrackById(int id) throws TrackNotFoundException{
         Track track = trackRepository.findById(id).get();
        return track;
     }
@@ -50,7 +50,7 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public Track updateTrack(int id,String comment) {
+    public Track updateTrack(int id,String comment) throws TrackNotFoundException{
         Optional<Track> track = trackRepository.findById(id);
         Track track1 = track.get();
         track1.setComment(comment);
@@ -60,12 +60,12 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public List<Track> getByTrackName(String name) {
+    public List<Track> getByTrackName(String name)throws TrackNotFoundException {
         return trackRepository.findByName(name);
     }
 
     @Override
-    public List<Track> getTrackByNameSortByName(String name) {
+    public List<Track> getTrackByNameSortByName(String name) throws TrackNotFoundException{
         return trackRepository.findByNameSortedById(name);
     }
 }
